62d485cae7dcfec0765a7974f44dd4d0446c7679
Jae-Hong-Kim <newfly101@naver.com>
2025-03-05 08:54:43 +0900

    [💄] 메인 페이지 생성

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,28 +4,14 @@ import "./App.css";
 import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
-    const [data, setData] = useState("");
-
-    useEffect(() => {
-        axios
-            .get('/api/data')
-            .then((res) => setData(res.data))
-            .catch((err) => console.log(err));
-    }, []);
-
-    const Main = () => (
-        <div>
-            <img src="./vite.svg" alt="logo"/>
-            <div>받아온 값 : {data}</div>
-        </div>
-    )
     return (
         <Router>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Main />}/>
+                    <Route path="/" element={<Home />}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/register" element={<Register/>}/>
                 </Route>
---
+++ web/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const Home = () => {
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        axios
+            .get('/api/data')
+            .then((res) => setData(res.data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            메인 페이지 입니다. (팀 코드 생성 및 참가 페이지)
+            <div>
+                <img src="./vite.svg" alt="logo"/>
+                <div>받아온 값 : {data}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
