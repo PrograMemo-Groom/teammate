73de490fa37434cbec1c6fd115bdad4791b25788
Jae-Hong-Kim <newfly101@naver.com>
2025-03-05 08:37:50 +0900

    [Router] 예시 추가

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,25 +1,35 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import Login from "./pages/Login.jsx";
 
 function App() {
-  const [data, setData] = useState("");
+    const [data, setData] = useState("");
 
-  useEffect(() => {
-    axios
-      .get('/api/data')
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+    useEffect(() => {
+        axios
+            .get('/api/data')
+            .then((res) => setData(res.data))
+            .catch((err) => console.log(err));
+    }, []);
 
-  return (
-    <>
-      <div>
-          <img src="./vite.svg" alt="logo" />
-        <div>받아온 값 : {data}</div>
-      </div>
-    </>
-  );
+    const Main = () => (
+        <div>
+            <img src="./vite.svg" alt="logo"/>
+            <div>받아온 값 : {data}</div>
+        </div>
+    )
+    return (
+        <Router>
+            <Routes>
+                <Route>
+                    <Route path="/" element={<Main />}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
