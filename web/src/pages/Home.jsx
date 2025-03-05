d5e1ca44e5fc43fda9020316153c3dfe3166697c
Jae-Hong-Kim <newfly101@naver.com>
2025-03-05 09:15:24 +0900

    [💄] 메인 페이지 - userNav 추가

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import Users from "./components/Users.jsx";
 
 const Home = () => {
     const [data, setData] = useState("");
@@ -13,6 +14,7 @@ const Home = () => {
 
     return (
         <div>
+            <Users />
             메인 페이지 입니다. (팀 코드 생성 및 참가 페이지)
             <div>
                 <img src="./vite.svg" alt="logo"/>
