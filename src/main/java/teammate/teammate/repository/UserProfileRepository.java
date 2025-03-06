72c98fbadf1294eeed6903d59e87881b4cb0ae54
happygirlll <lbm000314@gmail.com>
2025-03-06 17:18:21 +0900

    UserProfileRepository에 @Repository지정

--- src/main/java/teammate/teammate/repository/UserProfileRepository.java
+++ src/main/java/teammate/teammate/repository/UserProfileRepository.java
@@ -1,1 +1,1 @@
-package teammate.teammate.repository;import org.springframework.data.jpa.repository.JpaRepository;import teammate.teammate.domain.UserProfile;public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {}
+package teammate.teammate.repository;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;import teammate.teammate.domain.UserProfile;@Repositorypublic interface UserProfileRepository extends JpaRepository<UserProfile, Long> {}
