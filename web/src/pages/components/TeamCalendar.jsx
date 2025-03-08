37730fe18ea2bd13300edd646cbd608385fca62f
Jae-Hong-Kim <newfly101@naver.com>
2025-03-08 12:28:18 +0900

    [💄] mui calendar 추가

--- web/src/pages/components/TeamCalendar.jsx
+++ web/src/pages/components/TeamCalendar.jsx
@@ -1,23 +1,51 @@
-import React from 'react';
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import styles from "../../css/pages/Home.module.scss";
+import React, { useState } from "react";
+import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 const TeamCalendar = () => {
-    const [date, setDate] = React.useState(new Date());
+    const [date, setDate] = useState(dayjs());
+
+    const eventDates = ["2025-03-12", "2025-03-18", "2025-03-26"];
+
     return (
-        <div>
-            <h2>캘린더</h2>
-            <Calendar
-                onChange={setDate}
-                value={date}
-                locale="ko-KR"
-                tileContent={({date}) => {
-                    if ([12, 18, 26].includes(date.getDate())) {
-                        return <span style={{ color: "red" }}>●</span>;
-                    }
-                }}
-            />
-        </div>
+        <section>
+            <div className={styles.calendarContainer}>
+                <p>{dayjs().format('YYYY년 M월')}</p>
+                <button>+일정등록</button>
+            </div>
+
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <StaticDatePicker
+                    displayStaticWrapperAs="desktop"
+                    value={date}
+                    onChange={(newValue) => setDate(newValue)}
+                    slotProps={{
+                        day: (params) => {
+                            const formattedDate = dayjs(params.day).format("YYYY-MM-DD");
+
+                            if (eventDates.includes(formattedDate)) {
+                                return {
+                                    sx: {
+                                        position: "relative",
+                                        "&::after": {
+                                            content: '"●"',
+                                            color: "red",
+                                            fontSize: "12px",
+                                            position: "absolute",
+                                            bottom: -1,
+                                            left: "50%",
+                                            transform: "translateX(-50%)",
+                                        },
+                                    },
+                                };
+                            }
+                        },
+                    }}
+                />
+            </LocalizationProvider>
+        </section>
     );
 };
 
