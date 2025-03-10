62462f386af5dc69c0d7d82bbe11b0ba113e72c2
happygirlll <lbm000314@gmail.com>
2025-03-10 18:27:09 +0900

    기존 UserProfileRepository를 삭제하고 UsersRepository를 사용하도록 변경

--- src/main/java/teammate/teammate/repository/UserRepository.java
+++ src/main/java/teammate/teammate/repository/UserRepository.java
@@ -3,23 +3,25 @@ package teammate.teammate.repository;
 
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import teammate.teammate.domain.Users;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class UserRepository {
-    private final EntityManager em;
-
-    public Users getUserByUsername(int user_id){
-        String query = "select u from Users u";
-
-        List<Users> resultList = em.createQuery(query, Users.class).getResultList();
-
-        return resultList.get(0);
-    }
+public interface UserRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByUserId(String userId);
+//    private final EntityManager em;
+//
+//    public Users getUserByUsername(int user_id){
+//        String query = "select u from Users u";
+//
+//        List<Users> resultList = em.createQuery(query, Users.class).getResultList();
+//
+//        return resultList.get(0);
+//    }
 
 }
