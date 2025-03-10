e9a97c49437654330836d2c774ab803f98d42bc0
happygirlll <lbm000314@gmail.com>
2025-03-10 13:33:20 +0900

    User테이블에 Setter추가

--- src/main/java/teammate/teammate/domain/Users.java
+++ src/main/java/teammate/teammate/domain/Users.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigInteger;
 import java.util.List;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
