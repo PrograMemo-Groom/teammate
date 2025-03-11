a62cc7b3e2aacc86a1d367e636af1927650a9123
saaad99 <ehdgnstla@naver.com>
2025-03-12 03:44:51 +0900

    :art: HTTP 응답 코드 추가

--- src/main/java/teammate/teammate/controller/ApiResponse.java
+++ src/main/java/teammate/teammate/controller/ApiResponse.java
@@ -19,8 +19,13 @@ import java.util.Map;
 public class ApiResponse<T> {
 
     private static final int SUCCESS_STATUS = 200;
-    private static final int FAIL_STATUS = 404;
-    private static final int ERROR_STATUS = 500;
+    private static final int CREATED_STATUS = 201; // 리소스가 성공적으로 생성됨
+    private static final int NO_CONTENT_STATUS = 204; // 응답 내용 없음
+
+    private static final int BAD_REQUEST_STATUS = 400; // 잘못된 요청
+    private static final int UNAUTHORIZED_STATUS = 401; // 인증 필요
+    private static final int FORBIDDEN_STATUS = 403; // 권한 없음
+    private static final int NOT_FOUND_STATUS = 404; // 리소스를 찾을 수 없음
 
     private int status;
     private String message;
@@ -44,12 +49,12 @@ public class ApiResponse<T> {
 
     // 성공적으로 api 통신이 이루어졌지만 응답 데이터는 없을 때
     public static ApiResponse<?> createSuccessWithNoContent(String message) {
-        return new ApiResponse<>(SUCCESS_STATUS, message, null);
+        return new ApiResponse<>(NO_CONTENT_STATUS, message, null);
     }
 
     // 예외 발생으로 API 호출 실패시 반환
     public static ApiResponse<?> createError(String message) {
-        return new ApiResponse<>(ERROR_STATUS, message, null);
+        return new ApiResponse<>(NOT_FOUND_STATUS, message, null);
     }
 
 }
