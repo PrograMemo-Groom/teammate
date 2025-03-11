c240ce41c523bb98dc29b5e54f6cb6aac991245e
yoorym-kim <twilight99me@gmail.com>
2025-03-11 17:27:20 +0900

    🎨useState로 사용자 role이 OWNER일 때만 "+일정등록"버튼 렌더링하도록 설정(더미데이터)

--- web/src/pages/components/TeamCalendar.jsx
+++ web/src/pages/components/TeamCalendar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../css/pages/Home.module.scss";
 import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -6,20 +6,24 @@ import dayjs from "dayjs";
 import Schedule from "./Schedule.jsx";
 
 const TeamCalendar = ({ selectedDate, setSelectedDate }) => {
-    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [role, setRole] = useState("OWNER"); // 일단 더미 데이터, 나중에 API 연동코드로 변경 필요 !!!!!!!!!
+    //const [role, setRole] = useState("MEMBER");
 
     const handleModalOpen = (value) => {
         setIsOpen(value);
         console.log("modal open clicked", isOpen);
-    }
+    };
 
     const eventDates = ["2025-03-12", "2025-03-18", "2025-03-26"];
 
     return (
         <section>
             <div className={styles.calendarContainer}>
-                <p>{dayjs(selectedDate).format('YYYY년 M월')}</p>
-                <button onClick={() => handleModalOpen(true)}>+일정등록</button>
+                <p>{dayjs(selectedDate).format("YYYY년 M월")}</p>
+                {role === "OWNER" && (
+                    <button onClick={() => handleModalOpen(true)}>+일정등록</button>
+                )}
                 {isOpen && <Schedule onClose={() => setIsOpen(false)} />}
             </div>
 
@@ -27,7 +31,7 @@ const TeamCalendar = ({ selectedDate, setSelectedDate }) => {
                 <StaticDatePicker
                     displayStaticWrapperAs="desktop"
                     value={dayjs(selectedDate)}
-                    onChange={(newValue) => setSelectedDate(newValue.format("YYYY-MM-DD"))} // 선택한 날짜 변경
+                    onChange={(newValue) => setSelectedDate(newValue.format("YYYY-MM-DD"))}
                     slotProps={{
                         day: (params) => {
                             const formattedDate = dayjs(params.day).format("YYYY-MM-DD");
